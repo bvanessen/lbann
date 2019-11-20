7a6eae07239f41d1ef1dfbfbdae874fd02566391
Thomas R. Benson <benson31@llnl.gov>
2019-11-20 00:23:34 -0800

    finish layer_norm

--- include/lbann/layers/regularizers/layer_norm.hpp
+++ include/lbann/layers/regularizers/layer_norm.hpp
@@ -199,14 +199,14 @@ void layer_norm_layer<TensorDataType,Layout,Device>::bp_setup_gradient_wrt_input
 
 #ifndef LBANN_LAYER_NORM_LAYER_INSTANTIATE
 extern template class layer_norm_layer<
-  data_layout::DATA_PARALLEL, El::Device::CPU>;
+  DataType, data_layout::DATA_PARALLEL, El::Device::CPU>;
 extern template class layer_norm_layer<
-  data_layout::MODEL_PARALLEL, El::Device::CPU>;
+  DataType, data_layout::MODEL_PARALLEL, El::Device::CPU>;
 #ifdef LBANN_HAS_GPU
 extern template class layer_norm_layer<
-  data_layout::DATA_PARALLEL, El::Device::GPU>;
+  DataType, data_layout::DATA_PARALLEL, El::Device::GPU>;
 extern template class layer_norm_layer<
-  data_layout::MODEL_PARALLEL, El::Device::GPU>;
+  DataType, data_layout::MODEL_PARALLEL, El::Device::GPU>;
 #endif // LBANN_HAS_GPU
 #endif // LBANN_LAYER_NORM_LAYER_INSTANTIATE
 
